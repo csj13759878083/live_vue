3b967a90181e7ed785034f10d26f822996d0cdfc
csj13759878083 <576329379@qq.com>
2017-09-28 12:32:23 +0800

    cuishanjiang

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+  <Iheader></Iheader>
+  <Zuo></Zuo>
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import Iheader from './components/Header'
+import Zuo from './components/Zuo'
+import Main from './components/Main'
+export default {
+  name: 'app',
+  components:{
+    Iheader,Zuo,Main
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
